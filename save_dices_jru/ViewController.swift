0db2786d63b52bf58b2191f03c477c7997c55be3
jru <jieru04@gmail.com>
2022-10-03 08:46:12 +0800

    Initial Commit

---
+++ save_dices_jru/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  save_dices_jru
+//
+//  Created by jru on 2022/10/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
